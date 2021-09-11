3fa11653e0f0adc77be82cd0dd3c135895061515
tomGER <git@tomger.eu>
2021-09-11 23:16:21 +0200

    Add seperate clues file
    Closes #9

---
+++ clues.py
@@ -0,0 +1,69 @@
+###
+# Copyright 2021 tomGER, git@tomger.eu
+#
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with theLicence.
+#
+# You may obtain a copy of the Licence at: https://joinup.ec.europa.eu/software/page/eupl
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Licence for the specific language governing permissions and limitations under the Licence.
+###
+
+clues = {
+    "1": "The clue is in the description of /r/gamelevel2, which is Who?",
+    "2": "The clue is in the description of /r/gamelevel3. | What are you supposed to do on Reddit?",
+    "3": "The clue is in the description of /r/gamelevel4. The clue is We are PRO CSS! | Use old.reddit.com",
+    "4": "Make sure you have a QWERTY keyboard",
+    "5": "Pay close attention to the Reddit sidebar, and remember: two eyes.",
+    "6": "The clue is in the sidebar. It is mod vanity. | a spoiler tag in the post. Super major clue",
+    "7": "A site set up specifically for this level... hosted by the burning heavens.",
+    "8": "The most special things are out of reach sometimes.",
+    "9": "Clue is in the title. I can't see you. | But first let ...........",
+    "10": "Clue is the header image. There are also clues in the sidebar and blatantly in CSS.",
+    "11": "While the effect is best if you have RES, you could rely on a previous trick to find the answer.",
+    "12": "It's Shane is the clue",
+    "13": "So far you've been advancing through time... Now you need to roll it back to 0.",
+    "x": "It's a Übchi. The keyword is game.",
+    "14": "Review the first few levels. You may find there's a level you haven't been to yet.",
+    "15": "The date is one number off.",
+    "xx": "The clue is in the description of level 15.",
+    "16": "53 6f 20 79 6f 75 20 74 68 6f 75 67 68 74 20 74 68 61 74 20 79 6f 75 20 77 6f 75 6c 64 20 66 69 6e "
+            "64 20 74 68 65 20 61 6e 73 77 65 72 20 62 79 20 64 65 63 6f 64 69 6e 67 20 74 68 69 73 20 74 65 78 "
+            "74 20 62 75 74 20 74 68 61 74 20 69 73 20 62 75 6c 6c 73 68 69 74 2c 20 67 6f 20 62 61 63 6b 20 74 "
+            "6f 20 74 68 65 20 73 75 62 72 65 64 64 69 74 20 61 6e 64 20 6c 6f 6f 6b 20 44 45 45 50 20 69 6e 74 "
+            "6f 20 74 68 65 20 70 69 63 74 75 72 65 ",
+    "17": "Why might somebody use a program with an onion icon?",
+    "18": "hidden spoiler",
+    "19": "Secret Sauce",
+    "xxx": "A gif is stickied - Look at it :thinking:",
+    "20": "The clue is, Hard to h**ear** with a picture of an empty vase.",
+    "21": "I soametims do typos :thinking:",
+    "22": "One way to solve is to find /r/FutureGaming",
+    "23": "The clue is, who am I (zadoc) not?",
+    "24": "The clue is american wow.",
+    "25": "The clue is a reference to our Discord. Just ask the bot for the answer!",
+    "26": "The clue is a set of multicolored [!] in the sidebar",
+    "27": "The clue is, I cant believe we never used this one before. Also, in the description it says, naw.",
+    "272": "There are four RUE's, all the E's are red, and there's a pair of dice.",
+    "28": "Clue = Wat dis?",
+    "29": "The clue is: X - α4 = Y. Solve for Y. I give them that according to Zadoc",
+    "30": "What happened in 1997?",
+    "31": "Give no hints except 1, is our order! Sorry ;P",
+    "32": "Do the things my creator doesnt do often",
+    "33": "They have to decode the sound",
+    "34": "90's kids will remember text like this",
+    "35": "Ask a mod for hints to the numbers, this one is impossible for to answer as a bot - Be angry at Tom "
+            "for not giving me AI",
+    "36": "Idk, ask Zadoc or one of the winners",
+    "37": "Idk, ask Zadoc ***HA REUSED TEXT***",
+    "38": "Finish your level before looking at other levels",
+    "alpha": "https://cdn.discordapp.com/attachments/335895133805477889/436597671969554432/unknown.png",
+    "beta": "IDK, go bother <@307133814834987008>",
+    "gamma": "You once used this technique and now you will have to use it again",
+    "delta": "https://media.discordapp.net/attachments/497051392695861259/506424649262039040/image.gif",
+    "epsilon": "Clue",
+    "answer": "https://cdn.discordapp.com/attachments/365038956783599617/367011950858731541/Oshit.mp3",
+    "a": "A hint once hated now your best friend :) \n ```sudo ❏```"
+}
